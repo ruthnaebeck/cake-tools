9682b0edf819d7e411b9b939e5d658fcefd7f654
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-09-09 21:48:03 -0400

    Link replacement working

--- popup.js
+++ popup.js
@@ -15,9 +15,38 @@ function getCurrentTabUrl(callback) {
 }
 
 function replaceLinks() {
+  var regMatch = /https:\/\/(?!partlow|docs|help|www)\S*.datadoghq\S*/g;
+  var regReplace = /https\S*datadoghq.com/g;
   chrome.tabs.executeScript(null,
     {code: `
-      console.log('Links Replaced!');
+      chrome.storage.local.set({
+        'cakeToolStatus': 'No Links Found',
+      });
+      var orgId = '';
+      var nodeList = document.querySelectorAll('.ember-view.form_field.custom_field_25184603 input');
+      for(var x = 0; x < nodeList.length; x++) {
+        if (nodeList[x].value) {
+          orgId = nodeList[x].value;
+        }
+      }
+      if(orgId) {
+        var els = document.querySelectorAll('div.comment a');
+        for (var i = 0; i < els.length; i++) {
+          var linkText = els[i].innerText;
+          if(linkText.match(${regMatch})){
+            linkText = linkText.replace(${regReplace}, 'https://partlow.datadoghq.com/admin/switch_handle_get/org_id/' + orgId + '?next_url=');
+            els[i].href = linkText;
+            els[i].innerText = linkText;
+            chrome.storage.local.set({
+              'cakeToolStatus': 'Links Replaced!',
+            });
+          }
+        }
+      } else {
+        chrome.storage.local.set({
+          'cakeToolStatus': 'Please fill in the org_id field',
+        });
+      }
     `});
   setTimeout(function(){
     window.close();
@@ -31,8 +60,12 @@ function renderHTML(value) {
 document.addEventListener('DOMContentLoaded', function () {
   getCurrentTabUrl (function (url) {
     if (url.indexOf('datadog.zendesk.com/agent/tickets') > -1) {
-      replaceLinks();
-      renderHTML('Links Replaced!');
+      replaceLinks()
+      setTimeout(()=>{
+        chrome.storage.local.get(['cakeToolStatus'], function(result) {
+          renderHTML(result.cakeToolStatus);
+        })
+      }, 100);
     } else {
       renderHTML('Page Not Supported');
     }
